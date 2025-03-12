e1d2c3fb097f9150efe8ec1b8ce9fe5874a3caa3
simplyObare <josephatowiny@gmail.com>
2025-03-13 02:09:30 +0300

    creating cars app

--- Hyper Lux/cars/admin.py
+++ Hyper Lux/cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Feature, SuperCar, CarImage
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Feature)
+admin.site.register(SuperCar)
+admin.site.register(CarImage)
--- Hyper Lux/cars/models.py
+++ Hyper Lux/cars/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True, null=True)
 
@@ -10,8 +10,15 @@ class Category(models.Model):
         return self.name
 
 
-class SuperCar(models.Model):
+class Feature(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+
+class SuperCar(models.Model):
     # Basic details
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
@@ -23,6 +30,7 @@ class SuperCar(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, related_name="cars"
     )
+    features = models.ManyToManyField(Feature, related_name="cars")
 
     # Specifications
     engine = models.CharField(max_length=200)
@@ -41,17 +49,53 @@ class SuperCar(models.Model):
         ],
     )
 
-    # Media
-    exterior_image1 = models.ImageField(upload_to="supercars/exteriors/", blank=False)
-    exterior_image2 = models.ImageField(upload_to="supercars/exteriors/", blank=False)
-    exterior_image3 = models.ImageField(upload_to="supercars/exteriors/", blank=False)
-    exterior_image4 = models.ImageField(upload_to="supercars/exteriors/", blank=False)
-    interior_image = models.ImageField(upload_to="supercars/interiors/", blank=False)
-
     # Miscellaneous
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.make}-{self.model}-{self.year}")
+            slug = base_slug
+            counter = 1
+
+            while SuperCar.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.color} {self.make} {self.model} ({self.year})"
+
+
+class CarImage(models.Model):
+    class ImageCategory(models.TextChoices):
+        SIDE_VIEW = "Side View", "Side View"
+        DOORS_OPEN = "Doors Open", "Doors Open"
+        ENGINE = "Engine", "Engine"
+        INTERIOR = "Interior", "Interior"
+        FROM_BEHIND = "From Behind", "From Behind"
+
+    car = models.ForeignKey("SuperCar", on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="supercars/")
+    category = models.CharField(
+        max_length=50, choices=ImageCategory.choices, default=ImageCategory.SIDE_VIEW
+    )
+    is_exterior = models.BooleanField(default=True)
+    alt_text = models.CharField(
+        max_length=255, default="Image of an amazing supercar", editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        if self.car:
+            self.alt_text = (
+                f"{self.car.year} {self.car.make} {self.car.model} - {self.category}"
+            )
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.category} image of {self.car if self.car else 'Unknown Car'}"
